31f616159b0f65ae11b0814b8f37944b690a849d
yxz <www.1360542274@qq.com>
2019-08-27 17:48:38 +0800

    add sout v1.0

---
+++ src/main/java/com/baizhi/GithubApplication.java
@@ -0,0 +1,16 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApplication.class, args);
+        System.out.println("子鼠丑牛寅虎卯兔");
+        System.out.println("辰龙巳蛇午马未羊");
+        System.out.println("申猴酉鸡戌狗亥猪");
+    }
+
+}
